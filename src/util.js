f0d6820cbfa9286da06246350468e3e66ed9ea95
sevrijss <seppe.vanrijsselberghe@gmail.com>
2023-08-18 14:09:42 +0200

    fix linter no-prototype-builtins error

--- src/util.js
+++ src/util.js
@@ -14,7 +14,7 @@ export function shallowEqual(obj1, obj2) {
   }
 
   for (const key of keys1) {
-    if (!obj2.hasOwnProperty(key) || obj1[key] !== obj2[key]) {
+    if (!Object.hasOwn(obj2, key) || obj1[key] !== obj2[key]) {
       return false;
     }
   }
